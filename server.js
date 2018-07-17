834b7fd49550f666d4044ffc1c183295417d6a9a
Joe Wright <joewrightart@gmail.com>
2018-07-17 02:29:20 -0400

    heroku trial and error

--- server.js
+++ server.js
@@ -34,13 +34,13 @@ mongoose.connect(MONGODB_URI);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/static"));
+  app.use(express.static("client/build"));
 }
 
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/static/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, function() {
